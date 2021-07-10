081c952c3ba8c447248c9f61ccecf7b5abfdff6e
Yauheni Lebiadzevich <50967755+ZerGint@users.noreply.github.com>
2021-07-10 23:27:27 +0300

    класса Run общие наброски

--- Task2/src/run2/Run.java
+++ Task2/src/run2/Run.java
@@ -1,9 +1,38 @@
 package run2;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+
 public class Run {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		File file = new File("Task2\\resources\\inputFile.txt");
+		StringBuilder text = new StringBuilder();
+
+		try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+
+			String line;
+
+			while ((line = reader.readLine()) != null) {
+
+				// System.out.println(line);
+
+				text.append(line);
+
+			}
+
+		} catch (Exception e) {
+			System.out.println("error");
+			e.printStackTrace();
+		}
+		
+		
+		
+		
+		
+		System.out.println(text);
 
 	}
 
