0f11d4e66e5f94f25474ec74a25f702a4dd49e5d
Yauheni Lebiadzevich <50967755+ZerGint@users.noreply.github.com>
2021-07-10 23:26:52 +0300

    разработка класса TextEditor

--- Task2/src/entity2/TextEditor.java
+++ Task2/src/entity2/TextEditor.java
@@ -1,9 +1,40 @@
 package entity2;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class TextEditor {
 
+	List<TextPair> listTextPair = new ArrayList<>();;
+
 	public TextEditor() {
-		// TODO Auto-generated constructor stub
+
+	}
+
+	public void addToList(String text, int count) {
+
+		listTextPair.add(new TextPair(text, count));
+
+	}
+
+	private class TextPair implements Comparable<TextPair>{
+
+		String text;
+		int count;
+
+		private TextPair(String text, int count) {
+			this.text = text;
+			this.count = count;
+
+		}
+
+		public int compareTo(TextPair textPair) {
+			
+			return (this.count - textPair.count);
+		}
+		
+		
+
 	}
 
 }
